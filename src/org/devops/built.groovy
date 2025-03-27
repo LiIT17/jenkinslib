97f490bfa7b411d19344e381c2cacfc2d2cc57df
LiIT17 <17330887294@163.com>
2025-03-27 14:57:12 +0800

    Update built.groovy

--- src/org/devops/built.groovy
+++ src/org/devops/built.groovy
@@ -1,1 +1,8 @@
-packge 
+package org.devops
+
+def get-type(builttype){
+    def Toolstype = ["maven":"mvn","ant":"ant","gradle":"gradle","nodejs":"nmp" ]
+    println("当前构建类型为 ${builttype}")
+    tool = Toolstype[${builttype}]
+    sh '$tool $version'
+ }
